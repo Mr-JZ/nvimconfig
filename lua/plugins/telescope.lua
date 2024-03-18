e4f376f6f643bf486396fdb8247ff80e87b964c1
Jan Zisenis <jan.zisenis@gmail.com>
2024-03-18 12:12:45 +0100

    added Zotero for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,7 +14,50 @@ return  {
     build = "make",
     config = function()
       require("telescope").load_extension("fzf")
+      require("telescope").load_extension("zotero")
     end,
+    {
+      'jmbuhr/telescope-zotero',
+      dependencies = {
+        { 'kkharji/sqlite.lua' },
+      },
+      -- default opts shown
+      opts = {
+        zotero_db_path = '~/Zotero/zotero.sqlite',
+        better_bibtex_db_path = '~/Zotero/better-bibtex.sqlite',
+        -- specify options for different filetypes
+        -- locate_bib can be a string or a function
+        ft = {
+          quarto = {
+            insert_key_formatter = function(citekey)
+              return '@' .. citekey
+            end,
+            locate_bib = bib.locate_quarto_bib,
+          },
+          tex = {
+            insert_key_formatter = function(citekey)
+              return '\\cite{' .. citekey .. '}'
+            end,
+            locate_bib = bib.locate_tex_bib,
+          },
+          plaintex = {
+            insert_key_formatter = function(citekey)
+              return '\\cite{' .. citekey .. '}'
+            end,
+            locate_bib = bib.locate_tex_bib,
+          },
+          -- fallback for unlisted filetypes
+          default = {
+            insert_key_formatter = function(citekey)
+              return '@' .. citekey
+            end,
+            locate_bib = bib.locate_quarto_bib,
+          },
+        },
+
+            }
+        },
+    },
   },
   -- change some options
   opts = {
