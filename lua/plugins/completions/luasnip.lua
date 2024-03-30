4b527bca17bb7b34dfed6d8bee9d1e1e23d7dfd8
Jan Zisenis <mr-jz@fedora-2.fritz.box>
2024-03-30 10:51:48 +0100

    added completion to the nvim config

--- lua/plugins/completions/luasnip.lua
+++ lua/plugins/completions/luasnip.lua
@@ -1,5 +1,9 @@
 return {
   "L3MON4D3/LuaSnip",
+  depends = {
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+  },
   keys = function()
     return {}
   end,
--- lua/plugins/completions/nvim-cmp.lua
+++ lua/plugins/completions/nvim-cmp.lua
@@ -1,32 +1,58 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
+    "zbirenbaum/copilot-cmp",
     "hrsh7th/cmp-emoji",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = "copilot.lua",
-        opts = {},
-        config = function(_, opts)
-          local copilot_cmp = require("copilot_cmp")
-          copilot_cmp.setup(opts)
-          -- attach cmp source whenever copilot attaches
-          -- fixes lazy-loading issues with the copilot cmp source
-          LazyVim.lsp.on_attach(function(client)
-            if client.name == "copilot" then
-              copilot_cmp._on_insert_enter({})
-            end
-          end)
+    "Exafunction/codeium.nvim",
+  },
+  config = function()
+    local cmp = require("cmp")
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+          vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
         end,
       },
-      {
-        "Exafunction/codeium.nvim",
-        cmd = "Codeium",
-        build = ":Codeium Auth",
-        opts = {},
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
       },
-    },
-  },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+        -- { name = "nvim_lsp" },
+        { name = "luasnip" }, -- For luasnip users.
+      }, {
+        { name = "buffer" },
+      }),
+    })
+
+    -- Set configuration for specific filetype.
+    cmp.setup.filetype("gitcommit", {
+      sources = cmp.config.sources({
+        { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+      }, {
+        { name = "buffer" },
+      }),
+    })
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+  end,
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     table.insert(opts.sources, { name = "emoji" })
