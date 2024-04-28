5d08968c9cadb1bb75f0cc0e273e8cdebd15a032
Jan Zisenis <jan.zisenis@gmail.com>
2024-04-28 16:13:22 +0200

    change formatting of octo and also added the option to supress missing scope

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -1,12 +1,16 @@
-return {  'pwntester/octo.nvim',
+return {
+  "pwntester/octo.nvim",
   requires = {
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
     -- OR 'ibhagwan/fzf-lua',
-    'nvim-tree/nvim-web-devicons',
+    "nvim-tree/nvim-web-devicons",
   },
-  config = function ()
-    require"octo".setup()
-  end
+  config = function()
+    require("octo").setup({
+      suppress_missing_scope = {
+        projects_v2 = true,
+      },
+    })
+  end,
 }
-
