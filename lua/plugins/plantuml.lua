c100e8a3a645d8400b7725a869ae4aa3db844a10
Jan Zisenis <jan.zisenis@gmail.com>
2024-05-14 11:48:24 +0200

    implemented plantuml to compile for the nvim config

---
+++ lua/plugins/plantuml.lua
@@ -0,0 +1,17 @@
+return {
+  "https://gitlab.com/itaranto/plantuml.nvim",
+  tag = "v1.5.0",
+  config = function()
+    require("plantuml").setup({
+      renderer = {
+        type = "image",
+        options = {
+          prog = "eog",
+          dark_mode = true,
+          format = nil, -- Allowed values: nil, 'png', 'svg'.
+        },
+      },
+      render_on_write = true,
+    })
+  end,
+}
