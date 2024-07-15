0f8865c1482c2959a5242aaf2fad2cf4071a71c6
Jan Zisenis <mr-jz@Jans-Air.fritz.box>
2024-07-15 18:22:49 +0200

    changed to work with mac

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -13,7 +13,7 @@ return {
 
     " Viewer options: One may configure the viewer either by specifying a built-in
     " viewer method:
-    let g:vimtex_view_method = 'zathura'
+    let g:vimtex_view_method = 'skim'
 
     let g:vimtex_compiler_latexmk = {
         \ 'options' : [
