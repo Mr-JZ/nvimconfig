d7202202e1a5f43605bf892fa7f2ae805ffc3f4a
Jan Zisenis <mr-jz@Jans-Air.fritz.box>
2024-07-15 18:22:15 +0200

    changed short cut to work with mac

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,7 +12,7 @@ return {
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():add()
     end)
-    vim.keymap.set("n", "<C-e>", function()
+    vim.keymap.set("n", "<M-S-u>", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
