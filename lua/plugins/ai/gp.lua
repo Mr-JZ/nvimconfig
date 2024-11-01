f74a04b3ea61adf39d702a0bce083521bd7849d2
Jan Zisenis <jan.zisenis@gmail.com>
2024-11-01 14:46:10 +0100

    added avante and gp ai tools for awesome ai edeting experience

---
+++ lua/plugins/ai/gp.lua
@@ -0,0 +1,8 @@
+return {
+    "robitx/gp.nvim",
+    opts = {
+        -- Your configuration goes here, or leave it empty for default settings (see configuration section below)
+        openai_api_key = { "bw", "get", "password", "NVIM_OPENAI_KEY" },
+
+    }
+}
