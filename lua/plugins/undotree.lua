fbadc5a4d25cf505e4d563d907b46d5149bbbd9e
Jan Zisenis <jan.zisenis@gmail.com>
2024-12-07 10:07:37 +0100

    added description to the undotree shortcut

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,6 +2,6 @@ return {
   "mbbill/undotree",
   enabled = true,
   config = function()
-    vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
+    vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle, { desc = "Toggle undo tree"})
   end,
 }
