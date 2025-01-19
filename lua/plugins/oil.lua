2c1ef6dc1312ade65b91d4c917e7133ec89d31fe
Jan Zisenis <jan.zisenis@gmail.com>
2025-01-19 13:53:50 +0100

    implemented oil for fast dir editing

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  'stevearc/oil.nvim',
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  enabled = true,
+  opts = {},
+  -- Optional dependencies
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
