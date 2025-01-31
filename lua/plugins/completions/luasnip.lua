496f07bf8766d31227a3579a8d8ae9f7e27c90fa
Jan Zisenis <jan.zisenis@gmail.com>
2025-01-31 19:32:40 +0100

    change luasnip

--- lua/plugins/completions/luasnip.lua
+++ lua/plugins/completions/luasnip.lua
@@ -3,5 +3,9 @@ return {
 	-- follow latest release.
 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 	-- install jsregexp (optional!).
-	build = "make install_jsregexp"
+	build = "make install_jsregexp",
+  dependencies = {
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+  },
 }
