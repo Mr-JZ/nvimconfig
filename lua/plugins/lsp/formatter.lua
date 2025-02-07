1dcdc524e4b9c71f50b358c3b760cbb457f757c6
Jan Zisenis <jan.zisenis@gmail.com>
2025-02-07 10:04:17 +0100

    added conform for formatting and nvim-linter for lintting

---
+++ lua/plugins/lsp/formatter.lua
@@ -0,0 +1,29 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  enabled = true,
+  cmd = { "ConformInfo" },
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        go = { "gofumpt", "goimports-reviser", "golines" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+  end,
+}
