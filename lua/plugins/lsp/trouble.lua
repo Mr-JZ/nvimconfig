cddd1990d98a103b9a2c381c5d3e89c31037aa1c
Jan Zisenis <jan.zisenis@gmail.com>
2025-02-22 10:12:40 +0100

    added to toggle the diagnostic in the nvim window

--- lua/plugins/lsp/trouble.lua
+++ lua/plugins/lsp/trouble.lua
@@ -1,6 +1,20 @@
 return {
   "folke/trouble.nvim",
   dependencies = "nvim-tree/nvim-web-devicons",
+  keys = {
+    {
+      "<F3>",
+      function()
+        local trouble = require("trouble")
+        if next(vim.lsp.get_clients({ bufnr = 0 })) == nil then
+          vim.notify("No LSP clients are attached to this buffer", vim.log.levels.WARN)
+          return
+        end
+        trouble.toggle("diagnostics")
+      end,
+      desc = "Toggle Trouble Diagnostics",
+    },
+  },
   config = function()
     require("trouble").setup({
       -- your configuration comes here
