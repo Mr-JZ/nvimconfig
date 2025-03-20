d9454e6aaea4926b6280a375b8353e5511d5dc3b
Jan Zisenis <jan.zisenis@gmail.com>
2025-03-20 16:53:05 +0100

    added the nice move function to mark or delete sertande blocks

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,9 +2,50 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   opts = {
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    }
-    -- TODO: add treesitter movement: that means you can move to a function definition with ]m or select a function with [m
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+          ["ab"] = "@block.outer",
+          ["ib"] = "@block.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+          ["ai"] = "@conditional.outer",
+          ["ii"] = "@conditional.inner",
+          ["am"] = "@comment.outer",
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]]"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]M"] = "@function.outer",
+          ["]["] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[["] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[M"] = "@function.outer",
+          ["[]"] = "@class.outer",
+        },
+      },
+    },
+  },
 }
