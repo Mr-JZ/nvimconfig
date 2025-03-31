c1c2d90a41d0d1e5429651e8e0be37e10e059c8b
Jan Zisenis <jan.zisenis@gmail.com>
2025-03-31 10:30:35 +0200

    changed that i can't change the formatter quicker

--- lua/plugins/lsp/formatter.lua
+++ lua/plugins/lsp/formatter.lua
@@ -4,21 +4,25 @@ return {
   enabled = true,
   cmd = { "ConformInfo" },
   config = function()
+    ---@alias Formatter "prettier" | "eslint_d" | string
+    local formatter = "eslint_d"
+
     require("conform").setup({
       formatters_by_ft = {
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-        svelte = { "eslint_d" },
-        css = { "eslint_d" },
-        html = { "eslint_d" },
-        json = { "eslint_d" },
-        yaml = { "eslint_d" },
-        markdown = { "eslint_d" },
+        typescript = { formatter },
+        javascriptreact = { formatter },
+        typescriptreact = { formatter },
+        svelte = { formatter },
+        css = { formatter },
+        html = { formatter },
+        json = { formatter },
+        yaml = { formatter },
+        markdown = { formatter },
+        astro = { formatter },
         lua = { "stylua" },
         python = { "isort", "black" },
         go = { "gofumpt", "goimports-reviser", "golines" },
-        nix = { "nil" },
+        nix = { "nixfmt" },
       },
       format_on_save = {
         lsp_fallback = true,
