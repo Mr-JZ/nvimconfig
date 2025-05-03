10a569b5d66bbc4b517cdbb16c857530dd7a3cfe
Jan Zisenis <jan.zisenis@gmail.com>
2025-05-03 13:09:14 +0200

    added other specific model provider and models to avante

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -11,7 +11,43 @@ return {
     -- add any opts here
     -- for example
     ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-    provider = "gemini",
+    provider = "openai",
+    -- other config
+    -- The system_prompt type supports both a string and a function that returns a string. Using a function here allows dynamically updating the prompt with mcphub
+    openai = {
+      endpoint = "https://api.openai.com/v1",
+      model = "gpt-4.1",
+    },
+    claude = {
+      endpoint = "https://api.openai.com/v1",
+      model = "claude-3.5",
+    },
+    gemini = {
+      endpoint = "https://api.gemini.com/v1",
+      model = "gemini-2.5-pro",
+    },
+    system_prompt = function()
+      local hub = require("mcphub").get_hub_instance()
+      return hub:get_active_servers_prompt()
+    end,
+    -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+    custom_tools = function()
+      return {
+        require("mcphub.extensions.avante").mcp_tool(),
+      }
+    end,
+    disabled_tools = {
+      "list_files",
+      "search_files",
+      "read_file",
+      "create_file",
+      "rename_file",
+      "delete_file",
+      "create_dir",
+      "rename_dir",
+      "delete_dir",
+      "bash",
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
