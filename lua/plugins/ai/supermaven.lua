8d3d5a4da392b345b565b43937472a6ff05936d2
Jan Zisenis <jan.zisenis@gmail.com>
2025-05-07 19:10:50 +0200

    change the word accapt

--- lua/plugins/ai/supermaven.lua
+++ lua/plugins/ai/supermaven.lua
@@ -4,7 +4,7 @@ return {
     keymaps = {
       accept_suggestion = "<A-Space>",
       clear_suggestion = "<C-]>",
-      accept_word = "<A-j>",
+      accept_word = "<C-A-j>",
     },
     log_level = "info", -- set to "off" to disable logging completely
     disable_inline_completion = false, -- disables inline completion for use with cmp
