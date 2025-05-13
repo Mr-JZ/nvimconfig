651326ea1e45a290f3c94e65f3e1901a4cd23e52
Jan Zisenis <jan.zisenis@gmail.com>
2025-05-13 15:36:29 +0200

    disabled the package

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,7 +1,8 @@
 return {
   "iamcco/markdown-preview.nvim",
+  enabled = false,
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
+  build = "cd app && bun install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
   end,
