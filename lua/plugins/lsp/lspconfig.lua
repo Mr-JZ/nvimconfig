c3aca84034b097ef5f6f843bfe904b59e5979788
Jan Zisenis <jan.zisenis@gmail.com>
2025-05-13 15:37:49 +0200

    update the config

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -73,33 +73,19 @@ return {
       on_attach = on_attach,
     })
 
-    -- configure typescript server with plugin
-    lspconfig["ts_ls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      root_dir = util.root_pattern("package.json", "tsconfig.json"),
-    })
-    local ok, mason_registry = pcall(require, "mason-registry")
-    if not ok then
-      vim.notify("mason-registry could not be loaded")
-      return
-    end
-
-    local angularls_path = mason_registry.get_package("angular-language-server"):get_install_path()
+    -- -- configure typescript server with plugin
+    -- lspconfig["ts_ls"].setup({
+    --   capabilities = capabilities,
+    --   on_attach = on_attach,
+    -- })
 
     local cmd = {
       "ngserver",
       "--stdio",
       "--tsProbeLocations",
-      table.concat({
-        angularls_path,
-        vim.uv.cwd(),
-      }, ","),
+      "/home/mr-jz/.local/share/nvim/mason/packages/angular-language-server/node_modules/typescript/lib",
       "--ngProbeLocations",
-      table.concat({
-        angularls_path .. "/node_modules/@angular/language-server",
-        vim.uv.cwd(),
-      }, ","),
+      "/home/mr-jz/.local/share/nvim/mason/packages/angular-language-server/node_modules/@angular/language-server/bin",
     }
 
     lspconfig["angularls"].setup({
